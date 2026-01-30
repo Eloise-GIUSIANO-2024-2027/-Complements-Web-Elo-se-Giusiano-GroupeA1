028960c291f3cb53552b6702bde81d1939d2c394
Eloïse-GIUSIANO[2024-2027] <eloise.giusiano@etu.univ-amu.fr>
2026-01-30 15:34:01 +0100

    fin de l'étape 5

--- script.js
+++ script.js
@@ -8,14 +8,20 @@ function NombreAleatoire(I_min, I_max) {
   return Math.random() * (I_max - I_min) + I_min;
 }
 
-for (let i = 0; i < 20 ; i++) {
-  A_Tableau.push(Math.floor(NombreAleatoire(I_min, I_max)));
-}
-
 function NombreAleatoireDansLeTableau(A_Tableau){
   var ouais = Math.floor(Math.random() * A_Tableau.length);
   var rValue = A_Tableau[ouais];
   return rValue;
+}
+
+function Interval(){
+  for (let i = 0; i < 20 ; i++) {
+  document.getElementById("temperature2").textContent = A_Tableau[i];
+}
+}
+
+for (let i = 0; i < 20 ; i++) {
+  A_Tableau.push(Math.floor(NombreAleatoire(I_min, I_max)));
 }
 
 console.log(A_Tableau);
@@ -24,33 +30,35 @@ const log = document.getElementById("temperature").textContent;
 var nombre_fixe = NombreAleatoireDansLeTableau(A_Tableau)
 document.getElementById("temperature").textContent = nombre_fixe;
 
-function Interval(){
-  for (let i = 0; i < 20 ; i++) {
-  document.getElementById("temperature2").textContent = A_Tableau[i];
-}
-}
-
 var I_i = 0
+const LATEMP = document.getElementById('temperature2');
+
 setInterval(function() {
-  document.getElementById("temperature2").textContent = A_Tableau[I_i]; 
-
-  //const div1 = document.setAttribute("temperature2");
-  const tempe = document.querySelector("temperature2");
-  //var LAPAGE = document.getElementById("temperature2");
-  //const classs = LAPAGE.getAttribute("class");
-  //classs.className = "foo";
+  document.getElementById("temperature2").textContent = A_Tableau[I_i];  
   if (A_Tableau[I_i] < 0){
-  tempe.setAttribute("class","bleu");
-  } else if (A_Tableau[I_i] < 20){
-  tempe.setAttribute("class","vert");
-
-  }else if (A_Tableau[I_i] < 30){
-  tempe.setAttribute("class","orange");
-
-  }else {
-  tempe.setAttribute("class","rouge");
-
-  }
-  console.log(tempe.outerHTML);
-  I_i++ ;
+  LATEMP.classList.add('bleu');
+  LATEMP.classList.remove('orange'); 
+  LATEMP.classList.remove('vert');
+  LATEMP.classList.remove('rouge');
+}
+else if (A_Tableau[I_i] < 20){
+  LATEMP.classList.add('vert');
+  LATEMP.classList.remove('bleu');
+  LATEMP.classList.remove('orange');
+  LATEMP.classList.remove('rouge');
+}
+else if (A_Tableau[I_i] < 30){
+  LATEMP.classList.add('orange');
+  LATEMP.classList.remove('bleu');
+  LATEMP.classList.remove('vert');
+  LATEMP.classList.remove('rouge');
+}
+else {
+  LATEMP.classList.add('rouge');
+  LATEMP.classList.remove('bleu'); 
+  LATEMP.classList.remove('vert');
+  LATEMP.classList.remove('orange'); 
+}
+I_i++ ; 
 }, 2000);
+
